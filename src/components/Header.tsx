db4354fbc6fee981e662925ad1886fe1a6b0c903
Carter E Whitaker <138230291+CarterWhitaker@users.noreply.github.com>
2025-12-19 10:26:48 -0500

    Fixed hidden header search

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -90,7 +90,7 @@ export function Header({
                 <Search className="absolute left-3 top-1/2 -translate-y-1/2 text-gray-400 w-5 h-5" />
                 <input
                   type="search"
-                  placeholder="Search assets..."
+                  placeholder="Name, MAC Addr, or SN"
                   value={searchTerm}
                   onChange={(e) => onSearchChange(e.target.value)}
                   className="pl-10 pr-4 py-2 border border-gray-200 rounded-lg w-64 focus:outline-none focus:ring-2 focus:ring-[#87B812]"
